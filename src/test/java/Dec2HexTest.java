32c6c7ab7931fffb180710bc7ea53a79ad8f92f5
Callum Sherry <csherr200@caledonian.ac.uk>
2019-11-02 19:45:38 +0000

    added unit tests

--- src/test/java/Dec2HexTest.java
+++ src/test/java/Dec2HexTest.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import org.junit.Test;
 
 public class Dec2HexTest {
+
+    @Test
+    public void hexademicalTest() {
+        int testCode = Dec2Hex.convertToHexadecimal();
+        assert testCode == 0;
+    }
+
+
 }
