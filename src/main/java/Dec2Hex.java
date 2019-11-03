24f94208fed6778eef146be1ad9f3ded9a83015d
Callum Sherry <csherr200@caledonian.ac.uk>
2019-11-03 16:25:50 +0000

    Added logic to throw error when empty value is passed

--- src/main/java/Dec2Hex.java
+++ src/main/java/Dec2Hex.java
@@ -2,28 +2,31 @@ import java.util.Scanner;
 
 class Dec2Hex {
 
-    public static int Arg1;
+    private static int Arg1;
 
-    public static void main(String args[]) {
-        System.out.println("Hexadecimal representation is : " + convertToHexadecimal());
-
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hexadecimal representation is : " + convertToHexadecimal(getUserValue()));
     }
 
-    public static int convertToHexadecimal() {
+    public static String getUserValue() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Number You would Like to Convert To Hex: ");
+        return scanner.nextLine();
+    }
 
-        String hex = "5";
 
-
-        if (hex.length() > 0) {
-            try {
-                Arg1 = Integer.parseInt(hex);
-            } catch (NumberFormatException e) {
-                System.err.println("Argument" + hex + " must be an integer.");
-                System.exit(1);
-            }
+    public static int convertToHexadecimal(String hex) throws Exception {
+        if (hex == null || hex.length() == 0) {
+            throw new Exception("Please Insert a Value");
+        }
+        try {
+            Arg1 = Integer.parseInt(hex);
+        } catch (NumberFormatException e) {
+            System.err.println("Argument" + hex + " must be an integer.");
+            System.exit(1);
         }
 
-        char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
         int rem, num;
         num = Arg1;
         String hexadecimal = "";
--- src/test/java/Dec2HexTest.java
+++ src/test/java/Dec2HexTest.java
@@ -1,11 +1,19 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class Dec2HexTest {
 
+    @Mock
+    Dec2Hex dec2Hex;
+
     @Test
-    public void hexademicalTest() {
-        int testCode = Dec2Hex.convertToHexadecimal();
-        assert testCode == 0;
+    public void hexademicalTest() throws Exception {
+//        when(dec2Hex.convertToHexadecimal()).thenReturn();
     }
 
 
